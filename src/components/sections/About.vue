71ee6102919cecf0e0b9fe309a09a20110551412
Scheuer Patrik <scheuer.patrik@gmail.com>
2026-04-23 10:17:18 +0200

    Designing the "About me" section a bit

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,23 +1,32 @@
 <template>
     <div id="about" class="section about-section container">
         <div class="section-head">
-            <h3 class="section-head-title">About Me</h3>
+            <h3 class="section-head-title">&lt; About Me /&gt;</h3>
+            <hr>
         </div>
         <div class="section-body">
             <div class="about-container">
                 <div class="about-container-column">
-                    <div class="module-title">
-                        Developer.js
-                        <span>X</span>
+                    <div class="module-head">
+                        <h3 class="module-head-title">Developer.js</h3>
+                        <div class="module-head-buttons">
+                            <div class="module-head-button bg-[#ff605c]"> </div>
+                            <div class="module-head-button bg-[#ffbd44]"> </div>
+                            <div class="module-head-button bg-[#00ca4e]"> </div>
+                        </div>
                     </div>
                     <div class="module-body">
-                        <p>const Developer {
-                            }</p>
+                        <p>const Developer = require('./models/developer');</p>
+                        <br>
+                        <p>const spatrik2001 = new Developer ({<br>
+                            name: "Scheuer Patrik",
+                        })</p>
+
                     </div>
                 </div>
                 <div class="about-container-column">
-                    <div class="module-title">
-                        <img src="" alt="About me" class="about-container__thumbnail">
+                    <div class="module-head mb-2">
+                        <img src="https://szelearning.sze.hu/pluginfile.php/429195/user/icon/boost/f1?rev=18701322" alt="About me" class="about-container__thumbnail">
                     </div>
                     <div class="module-body">
                         <p>
